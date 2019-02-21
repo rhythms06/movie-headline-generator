f265044112592713b028cc079b07fb844427739a
Sakib Rasul <rhythms06@gmail.com>
2019-02-21 07:36:20 -0500

    Nested jQuery.when()

--- script.js
+++ script.js
@@ -64,62 +64,64 @@ $(document.body).ready(function () {
     //   console.log(errorMsg);
     // },
     // dataType: "text"
-  });
 
-  $.when(loadFile2).then(function(){
-    var loadFile3 = $.ajax({
-      url: "genres.txt",
-      async: true,
-      success: function (content) {
-        console.log("Found the genre database, parsing...");
-        // normalize the line breaks, then split into lines
-        lines = content.replace(/\r\n|\r/g, '\n').trim().split('\n');
-          
-        // only set up the click handler if there were lines found
-        if (lines && lines.length) {
-          $('#showLine').on('click', function () {
-            randomNumberG = parseInt(Math.random() * lines.length);
-            // // check to prevent infinite loop
-            // if (lines.length === 1) { break; }
-              
-            randomGenre = lines[randomNumberG];
-          });
-        }
-      }
-    });
-    // error: function(e) {
-    //   var errorMsg = e? (e.status + ' ' + e.statusText) : "";
-    //   console.log(errorMsg);
-    // },
-    // dataType: "text"
-  });
 
-  $.when(loadFile3).then(function(){
-    var loadFile4 = $.ajax({
-      url: "directors.txt",
-      async: true,
-      success: function (content) {
-        console.log("Found the director database, parsing...");
-        // normalize the line breaks, then split into lines
-        lines = content.replace(/\r\n|\r/g, '\n').trim().split('\n');
-    
-        // only set up the click handler if there were lines found
-        if (lines && lines.length) {
-          $('#showLine').on('click', function () {
-            randomNumberD = parseInt(Math.random() * lines.length);
-            // // check to prevent infinite loop
-            // if (lines.length === 1) { break; }
+    $.when(loadFile2).then(function(){
+      var loadFile3 = $.ajax({
+        url: "genres.txt",
+        async: true,
+        success: function (content) {
+          console.log("Found the genre database, parsing...");
+          // normalize the line breaks, then split into lines
+          lines = content.replace(/\r\n|\r/g, '\n').trim().split('\n');
             
-            randomDirector = lines[randomNumberD];
-          });
+          // only set up the click handler if there were lines found
+          if (lines && lines.length) {
+            $('#showLine').on('click', function () {
+              randomNumberG = parseInt(Math.random() * lines.length);
+              // // check to prevent infinite loop
+              // if (lines.length === 1) { break; }
+                
+              randomGenre = lines[randomNumberG];
+            });
+          }
         }
-      }
+      });
+      // error: function(e) {
+      //   var errorMsg = e? (e.status + ' ' + e.statusText) : "";
+      //   console.log(errorMsg);
+      // },
+      // dataType: "text"
+
+
+      $.when(loadFile3).then(function(){
+        var loadFile4 = $.ajax({
+          url: "directors.txt",
+          async: true,
+          success: function (content) {
+            console.log("Found the director database, parsing...");
+            // normalize the line breaks, then split into lines
+            lines = content.replace(/\r\n|\r/g, '\n').trim().split('\n');
+        
+            // only set up the click handler if there were lines found
+            if (lines && lines.length) {
+              $('#showLine').on('click', function () {
+                randomNumberD = parseInt(Math.random() * lines.length);
+                // // check to prevent infinite loop
+                // if (lines.length === 1) { break; }
+                
+                randomDirector = lines[randomNumberD];
+              });
+            }
+          }
+        });
+        // error: function(e) {
+        //   var errorMsg = e? (e.status + ' ' + e.statusText) : "";
+        //   console.log(errorMsg);
+        // },
+        // dataType: "text"
+      });
     });
-    // error: function(e) {
-    //   var errorMsg = e? (e.status + ' ' + e.statusText) : "";
-    //   console.log(errorMsg);
-    // },
-    // dataType: "text"
   });
 
   $('#showLine').on('click', function () {
