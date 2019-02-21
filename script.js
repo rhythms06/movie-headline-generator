f91f7dcf9029c603e31bcc2dfb46e662ed41f042
Sakib Rasul <rhythms06@gmail.com>
2019-02-21 05:42:18 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -11,32 +11,32 @@
 
 $(document.body).ready(function () {
   
-  // // load the trivia from the server
-  // $.ajax({
-  //   url: '/actors.txt'
-  // }).done(function(content) {
+  // load the trivia from the server
+  $.ajax({
+    url: 'actors.txt'
+  }).done(function(content) {
     
-  //   // normalize the line breaks, then split into lines
-  //   lines = content.replace(/\r\n|\r/g, '\n').trim().split('\n');
+    // normalize the line breaks, then split into lines
+    lines = content.replace(/\r\n|\r/g, '\n').trim().split('\n');
     
-  //   // only set up the click handler if there were lines found
-  //   if (lines && lines.length) {
-  //     $('#showLine').on('click', function () {
-  //       // loop to prevent repeating the last random number
-  //       while (randomNumberA === lastRandomNumber) {
-  //         randomNumberA = parseInt(Math.random() * lines.length);
-  //         // check to prevent infinite loop
-  //         if (lines.length === 1) { break; }
-  //       }
-  //       // keep track of the last random number
-  //       lastRandomNumber = randomNumberA;
+    // only set up the click handler if there were lines found
+    if (lines && lines.length) {
+      $('#showLine').on('click', function () {
+        // loop to prevent repeating the last random number
+        while (randomNumberA === lastRandomNumber) {
+          randomNumberA = parseInt(Math.random() * lines.length);
+          // check to prevent infinite loop
+          if (lines.length === 1) { break; }
+        }
+        // keep track of the last random number
+        lastRandomNumber = randomNumberA;
         
-  //       $('#headline').text(lines[randomNumberA]);
+        $('#headline').text(lines[randomNumberA]);
         
-  //     });
-  //   }
-  // });
+      });
+    }
+  });
 
-  $('#headline').text("TEST");
+  // $('#headline').text("TEST");
 
 });
